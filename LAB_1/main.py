a56981144e25d8d12aa0cfb8b034bde60c236426
STEPASEM <94398631+STEPASEM@users.noreply.github.com>
2025-11-27 16:14:21 +0300

    [*] инициализировал классы

--- LAB_1/main.py
+++ LAB_1/main.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 # Базовые исключения
 class SocialNetworkError(Exception):
     """Базовое исключение для социальной сети"""
@@ -21,25 +23,39 @@ class ValidationError(SocialNetworkError):
 
 
 class User:
-    def __init__(self, name):
-        pass
+    def __init__(self, user_id: int, username: str, email: str):
+        self.user_id = user_id
+        self.username = username
+        self.email = email
+        self.data_registration = datetime.now()
+        self.post: list[Post] = []
+        self.comment: list[Comment] = []
+
 
 class Post:
-    def __init__(self, text):
-        pass
+    def __init__(self, post_id: int, user_id: int, text: str):
+        self.post_id = post_id
+        self.user_id = user_id
+        self.text = text
+        self.created_at = datetime.now()
+        self.comment: list[Comment] = []
 
 class Comment:
-    def __init__(self, text):
-        pass
+    def __init__(self, comment_id: int, user_id: int, post_id: int, text: str):
+        self.user_id = user_id
+        self.post_id = post_id
+        self.text = text
+        self.created_at = datetime.now()
+
 
 class SocialNetwork:
     def __init__(self):
-        pass
+        self.users: dict[str, User] = {}
+        self.posts: dict[str, Post] = {}
 
 # Сериализация и десериализация
 class SocialNetworkSerializer:
-    def __init__(self):
-        pass
+    pass
 
 def main():
     pass
